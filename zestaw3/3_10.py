fce82e685f9c4f69b8dc4115a66fd68043d54b40
bananky <wojnarskap@gmail.com>
2022-10-29 13:03:59 +0200

    Zestaw 3 not finished

--- zestaw3/3_10.py
+++
@@ -1,7 +0,0 @@
-dict ={'I': '1','V':'5',"X":'10','L':'50','C':'100','D':'500','M':'1000'}
-x=input("Podaj liczbę w systemie rzymskim: ")
-x=x.split()
-for i in range(len(x)):
-    y = x[i]
-    z = dict[]
-    print(z)
